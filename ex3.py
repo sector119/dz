173c351ecee75a73735cbae684005b6f85a456cb
sector119 <sector119@eps.org.ua>
2020-09-30 10:11:47 +0300

    Ex 3 move 0 to the end of the list without sort + lambda

--- ex3.py
+++ ex3.py
@@ -1,3 +1,8 @@
 items = [0, 1, 2, 0, 0, 3, 0, 0, 4, 5, 0, 6, 0]
 
-print(items.sort(key=lambda item: item == 0))
+# print(items.sort(key=lambda item: item == 0))
+
+for i in range(0, len(items)):
+    items.append(items.pop(items.index(0)))
+
+print(items)
