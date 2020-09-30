cc3d712d2ba80ce30a62c1b0da599870556540ad
sector119 <sector119@eps.org.ua>
2020-09-30 11:29:47 +0300

    Ex 1 manual list population

--- ex1.py
+++ ex1.py
@@ -1,5 +1,14 @@
-c = input()
+n = int(input('Введіть к-ть елементів масиву: '))
 
-items = [0, 1, -1, 7, 12, -44, 8, -12, 4]
+items = []
+
+while n != 0:
+    item = input('-> ')
+
+    while not item.lstrip('-').isdigit():
+        item = input('Елемент не є числом, повторіть введення -> ')
+
+    items.append(int(item))
+    n -= 1
 
 print(max(items, key=abs))
