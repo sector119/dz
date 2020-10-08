cc349eae5d03d822c6bd0aa41cefcd99d8439c55
sector119 <sector119@eps.org.ua>
2020-10-08 15:14:39 +0300

    Ex 4-6

--- ex6.py
+++ ex6.py
@@ -1,9 +1,17 @@
+from itertools import zip_longest
 from math import atan, log
-
 from statistics import geometric_mean
-from numpy import arange
 
-n = float(input('Введіть n: '))
+from numpy import arange
+import matplotlib.pyplot as plt
+
+
+def grouper(n, iterable, padvalue=None):
+    return zip_longest(*[iter(iterable)] * n, fillvalue=padvalue)
+
+
+# n = float(input('Введіть n: '))
+n = 7
 
 a = 0.5
 b = 2.0
@@ -12,6 +20,19 @@ h = (b - a) / n
 
 f = 0.3
 
-data = filter(lambda y: y > f, [log(x) + atan(x) for x in arange(1, b, h)])
+cols = 2
+
+data = [y for y in [log(x) + atan(x) for x in arange(1, b, h)] if y > f]
 
 print(f'Середнє геометричне: {geometric_mean(data)}')
+
+print('Дані у вигляді таблиці')
+for row in grouper(cols, data, ''):
+    for col in row:
+        print(f'{str(col):>20}', end='')
+
+    print()
+
+plt.plot(data)
+plt.ylabel('y')
+plt.show()
