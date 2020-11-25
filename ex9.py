422a96d63f43ab21735deb323652c4c1091459ee
sector119 <sector119@eps.org.ua>
2020-11-25 10:47:34 +0200

    Ex 6

--- ex9.py
+++ ex9.py
@@ -1,15 +1,24 @@
+import time
+
+
+def show_train(train):
+    print(f"Number: {train['number']}")
+    print(f"Destination: {train['destination']}")
+    print(f"Departure time: {time.strftime('%H:%M', train['departure_time'])}")
+
+
 n = 3
 trains = []
 
 while n:
     t_number = input('Enter train number: ')
     t_destination = input('Enter train destination: ')
-    t_departure_time = input('Enter train departure time: ')
+    t_departure_time = input('Enter train departure time at H:M format: ')
 
     train = {
         'number': int(t_number),
         'destination': t_destination,
-        'departure_time': t_departure_time
+        'departure_time': time.strptime(t_departure_time, '%H:%M')
     }
 
     trains.append(train)
@@ -18,13 +27,14 @@ while n:
 
 trains.sort(key=lambda t: t['number'])
 
-print(trains)
+for train in trains:
+    show_train(train)
 
 number = int(input('Enter train number to show: '))
 
 for train in trains:
     if train['number'] == number:
-        print(train)
+        show_train(train)
         break
 else:
     print('Train not found')
